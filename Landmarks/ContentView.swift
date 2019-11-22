0cd6ba43ff42c19e40577b12c501d2d73566a1ea
lisafly2014 <lisafly2014@gmail.com>
2019-11-22 14:03:11 +1300

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Ximei Liu on 22/11/19.
+//  Copyright © 2019 WHSoftware. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
